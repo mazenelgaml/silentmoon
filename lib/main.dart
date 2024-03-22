0aedb015106b0f0c67d9fae826d3751114d8fcf2
Salma Elbasha <157319687+SalmaElbasha@users.noreply.github.com>
2024-03-22 05:39:39 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
-import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:taxico/screens/Otps/otps.dart';
+
+
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
 import 'package:taxico/screens/account_setting/account.dart';
-import 'package:taxico/screens/home/home_screen.dart';
-import 'package:taxico/screens/ride_history/ride.dart';
-void main() => runApp(MyApp());
+
+void main() {
+  runApp(MyApp());
+}
+
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+
       title: 'Flutter',
       debugShowCheckedModeBanner: false,
       home:Account(),
     );
   }
 }
+
+
+
+
+
