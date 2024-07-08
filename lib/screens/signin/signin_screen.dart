bd4c161ecbbba73ecb00c7d88149444671674333
mazenelgaml <112718739+mazenelgaml@users.noreply.github.com>
2024-07-09 01:41:07 +0300

    create sign in up

--- lib/screens/signin/signin_screen.dart
+++ lib/screens/signin/signin_screen.dart
@@ -1,23 +1,25 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../../widgets/custom_back_button/cutom_back_button.dart';
+import '../../widgets/custom_elevated_button/custom_elevated_button.dart';
 import '../../widgets/custom_text_form_field/custom_text_form_field.dart';
 
 class SigninScreen extends StatelessWidget {
-  const SigninScreen({super.key});
+  const SigninScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CustomBackButton(),
-          Center(
-            child: Container(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 40),
+            CustomBackButton(),
+            SizedBox(height: 20),
+            Center(
               child: Text(
                 "Welcome Back!",
                 style: TextStyle(
@@ -26,11 +28,112 @@ class SigninScreen extends StatelessWidget {
                 ),
               ),
             ),
-          ),
-          Center(
-            child: CustomTextFormField(text: 'Email address',),
-          )
-        ],
+            SizedBox(height: 30),
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xff8E97FD),
+                  borderRadius: BorderRadius.circular(38),
+                ),
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: CustomElevatedButton(
+                  onPressed: () {},
+                  text: 'CONTINUE WITH FACEBOOK',
+                  textColor: Colors.white,
+                  buttonColor: Color(0xff8E97FD),
+                ),
+              ),
+            ),
+            Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xff8E97FD),
+                  borderRadius: BorderRadius.circular(38),
+                ),
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: CustomElevatedButton(
+                  onPressed: () {},
+                  text: 'CONTINUE WITH GOOGLE',
+                  textColor: Colors.white,
+                  buttonColor: Color(0xff8E97FD),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: Text(
+                "OR LOG IN WITH EMAIL",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.w300,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: CustomTextFormField(
+                text: 'Email address',
+              ),
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: CustomTextFormField(
+                text: "Password",
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                decoration: BoxDecoration(
+                  color: Color(0xff8E97FD),
+                  borderRadius: BorderRadius.circular(38),
+                ),
+                child: CustomElevatedButton(
+                  onPressed: () {},
+                  text: 'LOG IN',
+                  textColor: Colors.white,
+                  buttonColor: Color(0xff8E97FD),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: Text(
+                "Forgot Password?",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "ALREADY HAVE AN ACCOUNT?",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 14,
+                  ),
+                ),
+                SizedBox(width: 5),
+                Text(
+                  "SIGN IN",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 14,
+                    color: Color(0xff8E97FD),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/signin_and_signup/signinup_screen.dart
+++ lib/screens/signin_and_signup/signinup_screen.dart
@@ -1,77 +1,101 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_utils/get_utils.dart';
+
+import '../../widgets/custom_elevated_button/custom_elevated_button.dart';
 
 class SigninupScreen extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body:Container(
-      width: Get.width,
-      height: Get.height,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-       Container(
-         width: Get.width,
-         child:  Image.asset("assets/images/logo.png",
-         fit: BoxFit.fill,),
-       ),
-        Center(
-          child: Container(
-            child: Text(
-              "We are what we do",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 30,
-              ),
-            ),
-          ),
-        ),
-        Center(
-          child: Container(
-            child: Text(
-              "Thousand of people are using silent moon \n                 for small meditation",
-              style: TextStyle(
-                fontWeight: FontWeight.w300, // or FontWeight.light
-                fontSize: 14
-              ),
-            ),
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-        child: Text(
-        "ALREADY HAVE AN ACCOUNT?",
-          style: TextStyle(
-            fontWeight: FontWeight.w500, // or FontWeight.medium
-            fontSize: 14,
-          ),
-        ),
-      ),
-            Container(
-              child: Text(
-                " LOG IN",
-                style: TextStyle(
-                  fontWeight: FontWeight.w500, // or FontWeight.medium
-                  fontSize: 14,
+      body: SafeArea(
+        child: Container(
+          width: Get.width,
+          height: Get.height,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: Get.width,
+                child: Image.asset(
+                  "assets/images/logo.png",
+                  fit: BoxFit.fill,
                 ),
               ),
-            ),
-
-          ],
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Center(
+                          child: Text(
+                            "We are what we do",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
+                          ),
+                        ),
+                        Center(
+                          child: Text(
+                            "Thousand of people are using silent moon \nfor small meditation",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w300,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                   SizedBox(height: 50,),
+                    Column(
+                      children: [
+                        Center(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Color(0xff8E97FD),
+                              borderRadius: BorderRadius.circular(38),
+                            ),
+                            child: CustomElevatedButton(
+                              onPressed: () {},
+                              text: 'SIGN UP',
+                              textColor: Colors.white,
+                              buttonColor: Color(0xff8E97FD),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "ALREADY HAVE AN ACCOUNT?",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 14,
+                              ),
+                            ),
+                            Text(
+                              " LOG IN",
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-
-
-        ],
       ),
-    ),
     );
   }
 }
